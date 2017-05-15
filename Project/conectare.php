049f14a41756d6c443e95155691ff9aa66b96b3a
TeamTI151 <darkhandratir@mail.ru>
2017-05-15 10:44:57 +0300

    Commit

--- Project/conectare.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conectare = mysqli_connect('localhost','root','','magazin');
-if(!$conectare)
-{
-	die('Conectarea la baza de date nu a reusit');
-}	
-?>
--- Project/conectare2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conectare = mysqli_connect('localhost','root','','autentificare');
-if(!$conectare)
-{
-	die('Conectarea la baza de date nu a reusit');
-}	
-?>
